47c427f4196bacd175e5d0a82395ea7ce2f7f4f2
Juila <julia@kanahoma.com>
2026-02-26 18:43:57 -0600

    no message

---
+++ app/public/.gitignore.c
@@ -0,0 +1,36 @@
+# WordPress core, config, and media
+/wp-config.php
+/wp-content/advanced-cache.php
+/wp-content/backup-db/
+/wp-content/backups/
+/wp-content/blogs.dir/
+/wp-content/cache/
+/wp-content/debug.log
+/wp-content/upgrade/
+/wp-content/uploads/
+/wp-content/wp-cache-config.php
+/wp-content/et-cache/
+/wp-content/gallery/
+/sitemap.xml
+/sitemap.xml.gz
+/web.config
+.htaccess
+
+# Theme development build artifacts and dependencies
+/wp-content/themes/teach-us/node_modules/
+/wp-content/themes/teach-us/vendor/
+/wp-content/themes/teach-us/npm-debug.log
+/wp-content/themes/teach-us/yarn-error.log
+/wp-content/themes/teach-us/dist/
+/wp-content/themes/teach-us/build/
+/wp-content/themes/teach-us/css/prod/
+/wp-content/themes/teach-us/js/prod/
+
+# OS generated files
+.DS_Store
+.DS_Store?
+._*
+.Spotlight-V100
+.Trashes
+ehthumbs.db
+Thumbs.db
